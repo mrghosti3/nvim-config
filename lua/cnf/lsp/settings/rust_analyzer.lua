60dc631d574f55e0d555b38c3b1ac06489b3be8b
mrghosti3 <mysteriousghst@gmail.com>
2023-05-09 12:00:24 +0300

    Minor adjustments to rust_analyzer config

--- lua/cnf/lsp/settings/rust_analyzer.lua
+++ lua/cnf/lsp/settings/rust_analyzer.lua
@@ -1,12 +1,26 @@
 return {
   settings = {
     rust_analyzer = {
+      cargo = {
+        buildScripts = {
+          enabled = true
+        },
+      },
       files = {
         excludeDirs = {
           ".git",
           "node_modules",
           "build",
         }
+      },
+      procMacro = {
+        enabled = true
+      },
+      imports = {
+        granularity = {
+          group = "module"
+        },
+        prefix = "self"
       }
     }
   }
