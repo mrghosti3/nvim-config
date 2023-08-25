4b3c8aa9a76cb9c2038ed081f3c8d769f7761d62
mrghosti3 <mysteriousghst@gmail.com>
2023-08-25 10:48:13 +0300

    Disabled ruby, perl integrations

--- lua/cnf/options.lua
+++ lua/cnf/options.lua
@@ -41,3 +41,6 @@ vim.cmd("syntax enable")
 
 vim.cmd("let g:loaded_python_provider = 0")
 vim.cmd("let g:python3_host_prog = '/usr/bin/python3'")
+
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
