2420d32b06dfc8e9b808e708317080b2643c3fd9
mrghosti3 <mysteriousghst@gmail.com>
2023-09-08 20:55:50 +0300

    Updat treesitter config

--- lua/cnf/treesitter.lua
+++ lua/cnf/treesitter.lua
@@ -4,25 +4,28 @@ if not status_ok then
 end
 
 configs.setup {
-  ensure_installed = { "c", "lua", "python", "php", "html", "javascript" },
+  ensure_installed = {
+    "c", "rust", "lua", "python",
+    "php", "html", "javascript"
+  },
   sync_install = false,
   ignore_install = { "haskell" }, -- List of parsers to ignore installing
   highlight = {
-    enable = true, -- false will disable the whole extension
-    disable = function (_lang, buf)
-      --[[ local disabled_langs = { "lua" } ]]
-      local max_file_size = 100 * 1024 -- 100 KB
+    enable = true,                -- false will disable the whole extension
+    disable = function(lang, buf)
+      local disabled_langs = { "html" }
+      local max_file_size = 10 * 1024 -- 10 KB
 
       local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
       if ok and stats and stats.size > max_file_size then
         return true
       end
 
-      --[[ for _i, val  in ipairs(disabled_langs) do ]]
-      --[[   if val == lang then ]]
-      --[[     return true ]]
-      --[[   end ]]
-      --[[ end ]]
+      for _, val in ipairs(disabled_langs) do
+        if val == lang then
+          return true
+        end
+      end
     end,
     additional_vim_regex_highlighting = true,
   },
