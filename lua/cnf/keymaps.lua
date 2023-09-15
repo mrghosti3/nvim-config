94260d39eaedf1435a03fe3d383210fd3fe1c0e2
mrghosti3 <mysteriousghst@gmail.com>
2023-09-15 23:31:51 +0300

    Switch resize shortcuts to more natural direction

--- lua/cnf/keymaps.lua
+++ lua/cnf/keymaps.lua
@@ -23,8 +23,8 @@ keymap("n", "<C-l>", "<C-w>l", opts)
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize +2<CR>", opts)
 keymap("n", "<C-Down>", ":resize -2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize +2<CR>", opts)
 
 keymap("n", "mf", ":lua vim.lsp.buf.format({async = true})<cr>", opts)
 
