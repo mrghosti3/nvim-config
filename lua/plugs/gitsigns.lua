26ef9edc6c345f32ace8045dcfd556174d9dd269
mrghosti3 <mysteriousghst@gmail.com>
2023-09-30 22:04:46 +0300

    Fix config styling: gitsings.lua

--- lua/plugs/gitsigns.lua
+++ lua/plugs/gitsigns.lua
@@ -1,18 +1,41 @@
-local gitsigns = require("gitsigns")
-
-gitsigns.setup {
-  signs = {
-    add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-    change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-    delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-  },
-  watch_gitdir = {
-    interval = 1000,
-    follow_files = true,
-  },
-  current_line_blame_formatter_opts = {
-    relative_time = false,
-  },
+require("gitsigns").setup {
+    signs = {
+        add = {
+            hl = "GitSignsAdd",
+            text = "▎",
+            numhl = "GitSignsAddNr",
+            linehl = "GitSignsAddLn"
+        },
+        change = {
+            hl = "GitSignsChange",
+            text = "▎",
+            numhl = "GitSignsChangeNr",
+            linehl = "GitSignsChangeLn"
+        },
+        delete = {
+            hl = "GitSignsDelete",
+            text = "契",
+            numhl = "GitSignsDeleteNr",
+            linehl = "GitSignsDeleteLn"
+        },
+        topdelete = {
+            hl = "GitSignsDelete",
+            text = "契",
+            numhl = "GitSignsDeleteNr",
+            linehl = "GitSignsDeleteLn"
+        },
+        changedelete = {
+            hl = "GitSignsChange",
+            text = "▎",
+            numhl = "GitSignsChangeNr",
+            linehl = "GitSignsChangeLn"
+        },
+    },
+    watch_gitdir = {
+        interval = 1000,
+        follow_files = true,
+    },
+    current_line_blame_formatter_opts = {
+        relative_time = false,
+    },
 }
