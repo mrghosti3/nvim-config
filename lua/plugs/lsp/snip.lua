6d11b0fef6c2184b928d5a330b72be05923dd52e
mrghosti3 <mysteriousghst@proton.me>
2024-04-26 10:35:07 +0300

    snip: optimize luasnip config

--- lua/plugs/lsp/snip.lua
+++ lua/plugs/lsp/snip.lua
@@ -1,20 +1,25 @@
 local luasnip = require("luasnip")
+local vscode_snip = require('luasnip.loaders.from_vscode')
+
 local options = {
   history = true,
   updateevents = "TextChanged,TextChangedI",
 }
 
 luasnip.config.set_config(options)
-require("luasnip.loaders.from_vscode").lazy_load()
-require("luasnip.loaders.from_vscode").lazy_load { paths = vim.g.luasnippets_path or "" }
+vscode_snip.lazy_load({
+  paths = { vim.g.luasnippets_path or "" }
+})
 
 vim.api.nvim_create_autocmd("InsertLeave", {
   callback = function()
-    if
-      require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
-      and not require("luasnip").session.jump_active
+    local lsnip = require('luasnip')
+    local session = lsnip.session
+    local current_buf = vim.api.nvim_get_current_buf()
+
+    if session.current_nodes[current_buf] and not session.jump_active
     then
-      require("luasnip").unlink_current()
+      lsnip.unlink_current()
     end
   end,
 })
