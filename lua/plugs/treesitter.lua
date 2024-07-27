4ed0d77c6930d2e5311550e4cd0906e5a17ae503
mrghosti3 <mysteriousghst@proton.me>
2024-07-27 15:59:42 +0300

    treesitter: add `markdown_inline` to `ensure_installed` list

--- lua/plugs/treesitter.lua
+++ lua/plugs/treesitter.lua
@@ -3,7 +3,7 @@ local ts_configs = require('nvim-treesitter.configs')
 ts_configs.setup({
   ensure_installed = {
     'c', 'vimdoc', 'rust', 'lua', 'python',
-    'html', 'javascript', 'zig', 'markdown'
+    'html', 'javascript', 'zig', 'markdown', 'markdown_inline'
   },
   sync_install = false,
   ignore_install = {},
