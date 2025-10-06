43020887569009ae799535f75671d37e2abd8d1e
mrghosti3 <mysteriousghst@proton.me>
2025-10-06 23:04:27 +0300

    feat(nvimtree): simplify config

--- lua/cnf/nvimtree.lua
+++ lua/cnf/nvimtree.lua
@@ -19,74 +19,72 @@ local nvim_tree = require('nvim-tree')
 --
 local function my_on_attach(bufnr)
     local api = require('nvim-tree.api')
-    local keymap = vim.keymap.set
-
-    local function opts(desc)
-        return {
+    local keymap = function(lhs, rhs, desc)
+        local opts = {
             desc = 'nvim-tree: ' .. desc,
             buffer = bufnr,
             noremap = true,
             silent = true,
             nowait = true
         }
+        vim.keymap.set('n', lhs, rhs, opts)
     end
 
-    keymap('n', '<2-RightMouse>', api.tree.change_root_to_node,       opts('CD RMouse'))
-    keymap('n', '<C-]>',          api.tree.change_root_to_node,       opts('CD'))
-    keymap('n', '<C-e>',          api.node.open.replace_tree_buffer,  opts('Open: In Place'))
-    keymap('n', '<C-k>',          api.node.show_info_popup,           opts('Info'))
-    keymap('n', '<C-r>',          api.fs.rename_sub,                  opts('Rename: Omit Filename'))
-    keymap('n', '<C-t>',          api.node.open.tab,                  opts('Open: New Tab'))
-    keymap('n', '<C-v>',          api.node.open.vertical,             opts('Open: Vertical Split'))
-    keymap('n', '<C-x>',          api.node.open.horizontal,           opts('Open: Horizontal Split'))
-    keymap('n', '<BS>',           api.node.navigate.parent_close,     opts('Close Directory'))
-    keymap('n', '<CR>',           api.node.open.edit,                 opts('Open'))
-    keymap('n', '<Tab>',          api.node.open.preview,              opts('Open Preview'))
-    keymap('n', '>',              api.node.navigate.sibling.next,     opts('Next Sibling'))
-    keymap('n', '<',              api.node.navigate.sibling.prev,     opts('Previous Sibling'))
-    keymap('n', '.',              api.node.run.cmd,                   opts('Run Command'))
-    keymap('n', '-',              api.tree.change_root_to_parent,     opts('Up'))
-    keymap('n', 'a',              api.fs.create,                      opts('Create'))
-    keymap('n', 'bmv',            api.marks.bulk.move,                opts('Move Bookmarked'))
-    keymap('n', 'B',              api.tree.toggle_no_buffer_filter,   opts('Toggle No Buffer'))
-    keymap('n', 'c',              api.fs.copy.node,                   opts('Copy'))
-    keymap('n', 'C',              api.tree.toggle_git_clean_filter,   opts('Toggle Git Clean'))
-    keymap('n', '[c',             api.node.navigate.git.prev,         opts('Prev Git'))
-    keymap('n', ']c',             api.node.navigate.git.next,         opts('Next Git'))
-    keymap('n', 'd',              api.fs.remove,                      opts('Delete'))
-    keymap('n', 'D',              api.fs.trash,                       opts('Trash'))
-    keymap('n', 'E',              api.tree.expand_all,                opts('Expand All'))
-    keymap('n', 'e',              api.fs.rename_basename,             opts('Rename: Basename'))
-    keymap('n', ']e',             api.node.navigate.diagnostics.next, opts('Next Diagnostic'))
-    keymap('n', '[e',             api.node.navigate.diagnostics.prev, opts('Prev Diagnostic'))
-    keymap('n', 'F',              api.live_filter.clear,              opts('Clean Filter'))
-    keymap('n', 'f',              api.live_filter.start,              opts('Filter'))
-    keymap('n', 'g?',             api.tree.toggle_help,               opts('Help'))
-    keymap('n', 'gy',             api.fs.copy.absolute_path,          opts('Copy Absolute Path'))
-    keymap('n', 'H',              api.tree.toggle_hidden_filter,      opts('Toggle Dotfiles'))
-    keymap('n', 'I',              api.tree.toggle_gitignore_filter,   opts('Toggle Git Ignore'))
-    keymap('n', 'J',              api.node.navigate.sibling.last,     opts('Last Sibling'))
-    keymap('n', 'K',              api.node.navigate.sibling.first,    opts('First Sibling'))
-    keymap('n', 'm',              api.marks.toggle,                   opts('Toggle Bookmark'))
-    keymap('n', 'o',              api.node.open.edit,                 opts('Open'))
-    keymap('n', '<2-LeftMouse>',  api.node.open.edit,                 opts('Open'))
-    keymap('n', 'O',              api.node.open.no_window_picker,     opts('Open: No Window Picker'))
-    keymap('n', 'p',              api.fs.paste,                       opts('Paste'))
-    keymap('n', 'P',              api.node.navigate.parent,           opts('Parent Directory'))
-    keymap('n', 'q',              api.tree.close,                     opts('Close'))
-    keymap('n', 'r',              api.fs.rename,                      opts('Rename'))
-    keymap('n', 'R',              api.tree.reload,                    opts('Refresh'))
-    keymap('n', 's',              api.node.run.system,                opts('Run System'))
-    keymap('n', 'S',              api.tree.search_node,               opts('Search'))
-    keymap('n', 'U',              api.tree.toggle_custom_filter,      opts('Toggle Hidden'))
-    keymap('n', 'W',              api.tree.collapse_all,              opts('Collapse'))
-    keymap('n', 'x',              api.fs.cut,                         opts('Cut'))
-    keymap('n', 'y',              api.fs.copy.filename,               opts('Copy Name'))
-    keymap('n', 'Y',              api.fs.copy.relative_path,          opts('Copy Relative Path'))
-
-    keymap('n', 'l',              api.node.open.edit,                 opts('Open'))
-    keymap('n', 'h',              api.node.navigate.parent_close,     opts('Close Directory'))
-    keymap('n', 'v',              api.node.open.vertical,             opts('Open: Vertical Split'))
+    keymap('<2-RightMouse>', api.tree.change_root_to_node,      'CD RMouse')
+    keymap('<2-LeftMouse>',  api.node.open.edit,                'Open')
+    keymap('<C-]>',          api.tree.change_root_to_node,      'CD')
+    keymap('<C-e>',          api.node.open.replace_tree_buffer, 'Open: In Place')
+    keymap('<C-k>',          api.node.show_info_popup,          'Info')
+    keymap('<C-r>',          api.fs.rename_sub,                 'Rename: Omit Filename')
+    keymap('<C-t>',          api.node.open.tab,                 'Open: New Tab')
+    keymap('<C-v>',          api.node.open.vertical,            'Open: Vertical Split')
+    keymap('<C-x>',          api.node.open.horizontal,          'Open: Horizontal Split')
+    keymap('<BS>',           api.node.navigate.parent_close,    'Close Directory')
+    keymap('<CR>',           api.node.open.edit,                'Open')
+    keymap('<Tab>',          api.node.open.preview,             'Open Preview')
+    keymap('>',              api.node.navigate.sibling.next,    'Next Sibling')
+    keymap('<',              api.node.navigate.sibling.prev,    'Previous Sibling')
+    keymap('.',              api.node.run.cmd,                  'Run Command')
+    keymap('-',              api.tree.change_root_to_parent,    'Up')
+    keymap('a',              api.fs.create,                     'Create')
+    keymap('bmv',            api.marks.bulk.move,               'Move Bookmarked')
+    keymap('B',              api.tree.toggle_no_buffer_filter,  'Toggle No Buffer')
+    keymap('c',              api.fs.copy.node,                  'Copy')
+    keymap('C',              api.tree.toggle_git_clean_filter,  'Toggle Git Clean')
+    keymap('[c',             api.node.navigate.git.prev,        'Prev Git')
+    keymap(']c',             api.node.navigate.git.next,        'Next Git')
+    keymap('d',              api.fs.remove,                     'Delete')
+    keymap('D',              api.fs.trash,                      'Trash')
+    keymap('E',              api.tree.expand_all,               'Expand All')
+    keymap('e',              api.fs.rename_basename,            'Rename: Basename')
+    keymap(']e',             api.node.navigate.diagnostics.next,'Next Diagnostic')
+    keymap('[e',             api.node.navigate.diagnostics.prev,'Prev Diagnostic')
+    keymap('F',              api.live_filter.clear,             'Clean Filter')
+    keymap('f',              api.live_filter.start,             'Filter')
+    keymap('g?',             api.tree.toggle_help,              'Help')
+    keymap('gy',             api.fs.copy.absolute_path,         'Copy Absolute Path')
+    keymap('H',              api.tree.toggle_hidden_filter,     'Toggle Dotfiles')
+    keymap('I',              api.tree.toggle_gitignore_filter,  'Toggle Git Ignore')
+    keymap('J',              api.node.navigate.sibling.last,    'Last Sibling')
+    keymap('K',              api.node.navigate.sibling.first,   'First Sibling')
+    keymap('m',              api.marks.toggle,                  'Toggle Bookmark')
+    keymap('o',              api.node.open.edit,                'Open')
+    keymap('O',              api.node.open.no_window_picker,    'Open: No Window Picker')
+    keymap('p',              api.fs.paste,                      'Paste')
+    keymap('P',              api.node.navigate.parent,          'Parent Directory')
+    keymap('q',              api.tree.close,                    'Close')
+    keymap('r',              api.fs.rename,                     'Rename')
+    keymap('R',              api.tree.reload,                   'Refresh')
+    keymap('s',              api.node.run.system,               'Run System')
+    keymap('S',              api.tree.search_node,              'Search')
+    keymap('U',              api.tree.toggle_custom_filter,     'Toggle Hidden')
+    keymap('W',              api.tree.collapse_all,             'Collapse')
+    keymap('x',              api.fs.cut,                        'Cut')
+    keymap('y',              api.fs.copy.filename,              'Copy Name')
+    keymap('Y',              api.fs.copy.relative_path,         'Copy Relative Path')
+    keymap('l',              api.node.open.edit,                'Open')
+    keymap('h',              api.node.navigate.parent_close,    'Close Directory')
+    keymap('v',              api.node.open.vertical,            'Open: Vertical Split')
 end
 
 nvim_tree.setup {
@@ -97,10 +95,10 @@ nvim_tree.setup {
     diagnostics = {
         enable = true,
         icons = {
-            hint = "",
-            info = "",
-            warning = "",
-            error = "",
+            hint = "H",
+            info = "I",
+            warning = "W",
+            error = "E",
         },
     },
     update_focused_file = {
