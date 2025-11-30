305bd4bbbe29add0293c7f65dcbefff82818bc54
mrghosti3 <mysteriousghst@proton.me>
2025-12-01 01:03:34 +0200

    chore: update plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -70,7 +70,7 @@ return {
     },
     {
         'nvim-telescope/telescope.nvim',
-        version = "^0.1.8",
+        version = "^0.2.0",
         event = { "VeryLazy", },
         dependencies = {
             'nvim-lua/plenary.nvim'
