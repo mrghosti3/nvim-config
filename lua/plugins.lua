7893262be6832180569655accb59b1e7999f8f71
mrghosti3 <mysteriousghst@proton.me>
2026-06-23 23:27:39 +0300

    fix: move Comment.nvim config to plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -101,9 +101,23 @@ return {
     {
         'numToStr/Comment.nvim',
         lazy = false,
-        config = function()
-            require('cnf.comment')
-        end
+        opts = {
+            pre_hook = function(ctx)
+                local U = require "Comment.utils"
+
+                local location = nil
+                if ctx.ctype == U.ctype.block then
+                  location = require("ts_context_commentstring.utils").get_cursor_location()
+                elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+                  location = require("ts_context_commentstring.utils").get_visual_start_location()
+                end
+
+                return require("ts_context_commentstring.internal").calculate_commentstring {
+                  key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
+                  location = location,
+                }
+            end
+        },
     }, -- Commenting shortcuts
     {
         'nmac427/guess-indent.nvim',
