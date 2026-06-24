e4688849be8f669ca8dc81b1c26aa57da712cd4e
mrghosti3 <mysteriousghst@proton.me>
2026-06-24 21:39:27 +0300

    feat: new command for active LSPs quickview
    Introduces new command to quickly check for active LSPs, without going
    into `checkhealth`.

--- lua/cnf/comm.lua
+++ lua/cnf/comm.lua
@@ -23,3 +23,46 @@ vim.api.nvim_create_user_command(
         desc = 'Delete buffer without closing window'
     }
 )
+
+vim.api.nvim_create_user_command(
+    "Lspls",
+    function(opts)
+        local args = opts.fargs
+        local bufs = { }
+
+        if #args > 0 then
+            for _, b in ipairs(args) do
+                local buf, ok = pcall(vim.fn.bufnr, b, false)
+                if ok and buf ~= -1 then
+                    table.insert(bufs, b)
+                end
+            end
+        else
+            for _, b in ipairs(vim.api.nvim_list_bufs()) do
+                if vim.bo[b].buflisted == true then
+                    table.insert(bufs, b)
+                end
+            end
+        end
+
+        local lsps = {}
+        for _, l in ipairs(vim.lsp.get_clients()) do
+            for _, b in ipairs(bufs) do
+                if l.attached_buffers[b] then
+                    if lsps[l.name] == nil then
+                        lsps[l.name] = {}
+                    end
+
+                    table.insert(lsps[l.name], b)
+                end
+            end
+        end
+        vim.print(lsps)
+    end,
+    {
+        nargs = '*',
+        bang = false,
+        complete = 'buffer',
+        desc = 'Delete buffer without closing window'
+    }
+)
